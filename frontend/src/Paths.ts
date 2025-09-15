70d3e704fa46bfb42cc99d63b16ed54742b1a14f
DavideBran <bdavide3@icloud.com>
2025-09-15 22:58:58 +0200

    Renamed player and map files

---
+++ frontend/src/Paths.ts
@@ -0,0 +1,4 @@
+
+export const PLAYER_PATH = "./src/assets/player";
+export const MAP_PATH = "./src/assets/maps";
+export const TILESET_PATH = "./src/assets/maps/tileset";
--- frontend/src/game/scenes/BootScene.ts
+++ frontend/src/game/scenes/BootScene.ts
@@ -1,63 +1,54 @@
 import Phaser from "phaser";
+import { MAP_PATH, PLAYER_PATH, TILESET_PATH } from "../../Paths";
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
     super("BootScene");
   }
 
-  preload() {
+  private loadPlayerSprite() {
     // Idle
-    this.load.spritesheet(
-      "player_idle",
-      "./src/assets/player/player_idle_animation.png",
-      {
-        frameWidth: 36,
-        frameHeight: 44,
-      }
-    );
+    this.load.spritesheet("player_idle", `${PLAYER_PATH}/idle.png`, {
+      frameWidth: 36,
+      frameHeight: 44,
+    });
 
     // Left walk
-    this.load.spritesheet(
-      "player_walk_left",
-      "./src/assets/player/player_walk_left_animation.png",
-      {
-        frameWidth: 38,
-        frameHeight: 44,
-      }
-    );
+    this.load.spritesheet("player_walk_left", `${PLAYER_PATH}/left.png`, {
+      frameWidth: 38,
+      frameHeight: 44,
+    });
 
     // Right walk
-    this.load.spritesheet(
-      "player_walk_right",
-      "./src/assets/player/player_walk_right_animation.png",
-      {
-        frameWidth: 29,
-        frameHeight: 44,
-      }
-    );
+    this.load.spritesheet("player_walk_right", `${PLAYER_PATH}/right.png`, {
+      frameWidth: 29,
+      frameHeight: 44,
+    });
 
     // Down walk
-    this.load.spritesheet(
-      "player_walk_down",
-      "./src/assets/player/player_walk_down_animation.png",
-      {
-        frameWidth: 24,
-        frameHeight: 44,
-      }
-    );
-    
+    this.load.spritesheet("player_walk_down", `${PLAYER_PATH}/down.png`, {
+      frameWidth: 24,
+      frameHeight: 44,
+    });
+
     // Up walk
-    this.load.spritesheet(
-      "player_walk_up",
-      "./src/assets/player/player_walk_up_animation.png",
-      {
-        frameWidth: 25,
-        frameHeight: 43,
-      }
-    );
+    this.load.spritesheet("player_walk_up", `${PLAYER_PATH}/up.png`, {
+      frameWidth: 25,
+      frameHeight: 43,
+    });
+  }
+
+  private loadContents() {
+    this.load.image("brucoliMap", `${MAP_PATH}/brucoli.png`);
+    this.load.json("brucoliCollision", `${TILESET_PATH}/brucoli.json`);
+  }
+
+  preload() {
+    this.loadPlayerSprite();
+    this.loadContents();
   }
 
   create() {
-    this.scene.start("MainScene");
+    this.scene.start("BrucoliScene");
   }
 }
